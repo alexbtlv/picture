f0eb57519dc06f5b67935d6161a30465c862aa29
Alexander Batalov <alexbtlv@gmail.com>
2014-08-17 19:52:33 +0600

    test preps

---
+++ spec/factories/images.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :image do
+  	title "title"
+  	description "Image description goes here."
+  	remote_asset_url "http://cdn1.vox-cdn.com/assets/4203957/vox_placeholder.jpg"
+  end
+end
